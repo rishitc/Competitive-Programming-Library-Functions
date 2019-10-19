e460db70ae5e8f1ceaa12b1546c1b4d493525d2e
unknown <akshatdixit51@gmail.com>
2019-10-19 14:32:48 +0530

    updated file as per your requirements

--- Linked Lists/Doubly Linked Lists/Function.cpp
+++ Linked Lists/Doubly Linked Lists/Function.cpp
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<iostream>
+using namespace std;
 struct Node
 {
 	int data;
-	struck Node *next;
-	struck Node *prev;
+	struct Node *next;
+	struct Node *prev;
 };
 
 void push(struct Node** head_ref, int new_data)
@@ -214,3 +215,72 @@ Node *mergeSort(Node *head)
     return merge(head,second);  
 }  
 
+//added swap node functionality by changing pointers instead of data
+
+void swapNodes(Node **head_ref, int x, int y)  
+{  
+ 
+if (x == y) return;  
+  
+  
+Node *prevX = NULL, *currX = *head_ref;  
+while (currX && currX->data != x)  
+{  
+    prevX = currX;  
+    currX = currX->next;  
+}  
+  
+  
+Node *prevY = NULL, *currY = *head_ref;  
+while (currY && currY->data != y)  
+{  
+    prevY = currY;  
+    currY = currY->next;  
+}  
+  
+  
+if (currX == NULL || currY == NULL)  
+    return;  
+  
+ 
+if (prevX != NULL)  
+    prevX->next = currY;  
+else  
+    *head_ref = currY;  
+  
+  
+if (prevY != NULL)  
+    prevY->next = currX;  
+else 
+    *head_ref = currX;  
+  
+
+Node *temp = currY->next;  
+currY->next = currX->next;  
+currX->next = temp;  
+}
+
+
+//added main function to check the swap functionality of the pointer nodes
+int main()  
+{  
+    Node *start = NULL;  
+  
+    push(&start, 7);  
+    push(&start, 6);  
+    push(&start, 5);  
+    push(&start, 4);  
+    push(&start, 3);  
+    push(&start, 2);  
+    push(&start, 1);  
+  
+    cout << "Linked list before calling swapNodes() ";  
+    printList(start);  
+  
+    swapNodes(&start, 4, 3);  
+  
+    cout << "\nLinked list after calling swapNodes() ";  
+    printList(start);  
+  
+    return 0;  
+}  
